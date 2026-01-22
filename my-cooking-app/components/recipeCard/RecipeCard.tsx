632ed7bc48dd24eaebe675685d491530d9b4b451
DominkoMilic <dmilic04@fesb.hr>
2026-01-22 18:35:15 +0100

    feat: Remove unused imports and back navigation button from recipe page, improve recipe card styling

--- my-cooking-app/components/recipeCard/RecipeCard.tsx
+++ my-cooking-app/components/recipeCard/RecipeCard.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import { Recipe } from "./RecipeCard.types";
 import { RecipeCardSkeleton } from "./RecipeSkeletonLoaderCard";
@@ -11,6 +12,7 @@ import CakeImage from "../../public/assets/cake.png";
 import SoupImage from "../../public/assets/soup.png";
 import { HeartIcon } from "@/public/reactComponentAssets/HeartIcon";
 import { BookmarkIcon } from "@/public/reactComponentAssets/BookmarkIcon";
+import backArrow from "../../public/assets/backArrow.png";
 
 export function RecipeCard({
   recipe,
@@ -19,19 +21,26 @@ export function RecipeCard({
   recipe?: Recipe;
   isLoading?: boolean;
 }) {
+  const router = useRouter();
+
   if (isLoading || !recipe) return <RecipeCardSkeleton />;
 
   return (
-    <article className="w-[360px] bg-section-bg rounded-3xl p-8 shadow-md text-body-text flex flex-col gap-6">
+    <article className="w-full max-w-[360px] md:max-w-[720px] bg-section-bg rounded-3xl p-8 shadow-md text-body-text flex flex-col gap-6">
       {/* Header */}
       <header className="flex items-center gap-3">
-        <Image
-          src={LemonImage}
-          alt="Lemon icon"
-          width={50}
-          height={50}
-          className="rounded-md"
-        />
+        <button
+          onClick={() => router.back()}
+          className="cursor-pointer transition duration-200 hover:scale-110 hover:opacity-80"
+        >
+          <Image
+            src={backArrow}
+            alt="Back"
+            width={48}
+            height={48}
+            className="w-15 md:w-12 aspect-square"
+          />
+        </button>
         <h1 className="text-3xl font-bold text-primary-text font-playfair">
           {recipe.title}
         </h1>
@@ -42,55 +51,57 @@ export function RecipeCard({
         {recipe.description}
       </p>
 
-      {/* Ingredients */}
-      <section className="flex justify-between items-start">
-        <div>
-          <h2 className="text-xl font-semibold mb-2 text-primary-text font-playfair">
-            Ingredients
-          </h2>
-          <ul className="list-disc ml-5 text-sm">
-            {recipe.ingredients.map((item, i) => (
-              <li key={i} className="mb-1 text-body-text">
-                {item}
-              </li>
-            ))}
-          </ul>
+      {/* Ingredients & Instructions */}
+      <section className="flex flex-col md:flex-row items-start gap-6">
+        <div className="flex items-start justify-center">
+          {/* Ingredients */}
+          <div className="md:w-1/2 flex flex-col gap-4">
+            <h2 className="text-xl font-semibold mb-2 text-primary-text font-playfair">
+              Ingredients
+            </h2>
+            <ul className="list-disc ml-5 text-sm flex-1">
+              {recipe.ingredients.map((item, i) => (
+                <li key={i} className="mb-1 text-body-text">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="w-24 h-20 rounded-xl flex items-center justify-center">
+            <Image
+              src={CakeImage}
+              alt="Cake"
+              width={90}
+              height={90}
+              className="object-contain -rotate-12"
+            />
+          </div>
         </div>
 
-        <div className="w-24 h-20 rounded-xl flex items-center justify-center">
-          <Image
-            src={CakeImage}
-            alt="Cake"
-            width={90}
-            height={90}
-            className="object-contain -rotate-12"
-          />
-        </div>
-      </section>
-
-      {/* Instructions */}
-      <section className="flex justify-between items-start">
-        <div>
-          <h2 className="text-xl font-semibold mb-2 text-primary-text font-playfair">
-            Instructions
-          </h2>
-          <ol className="list-decimal ml-5 text-sm">
-            {recipe.instructions.map((step, i) => (
-              <li key={i} className="mb-1 text-body-text">
-                {step}
-              </li>
-            ))}
-          </ol>
-        </div>
-
-        <div className="w-24 h-20 rounded-xl flex items-center justify-center">
-          <Image
-            src={SoupImage}
-            alt="Soup"
-            width={80}
-            height={80}
-            className="object-contain rotate-20"
-          />
+        {/* Instructions */}
+        <div className="flex justify-center items-start">
+          <div className="md:w-1/2 flex flex-col gap-4">
+            <h2 className="text-xl font-semibold mb-2 text-primary-text font-playfair">
+              Instructions
+            </h2>
+            <ol className="list-decimal ml-5 text-sm flex-1">
+              {recipe.instructions.map((step, i) => (
+                <li key={i} className="mb-1 text-body-text">
+                  {step}
+                </li>
+              ))}
+            </ol>
+          </div>
+          <div className="w-24 h-20 rounded-xl flex items-center justify-center">
+            <Image
+              src={SoupImage}
+              alt="Soup"
+              width={80}
+              height={80}
+              className="object-contain rotate-20"
+            />
+          </div>
         </div>
       </section>
 
@@ -106,40 +117,20 @@ export function RecipeCard({
         </div>
       )}
 
+      {/* Footer */}
       <footer className="flex justify-between items-center text-sm text-body-text">
         <span className="text-body-text">{recipe.author}</span>
-
-        {/* Icons container */}
         <div className="flex flex-col sm:flex-row items-center gap-5">
           {/* Likes */}
           <div className="flex items-center gap-1 text-body-text">
-            <button
-              className="
-          w-8 h-8 
-          flex items-center justify-center
-          hover:text-primary-text
-          cursor-pointer
-          transition-all duration-200
-          hover:scale-110 active:scale-95
-        "
-            >
+            <button className="w-8 h-8 flex items-center justify-center hover:text-primary-text cursor-pointer transition-all duration-200 hover:scale-110 active:scale-95">
               <HeartIcon className="w-5 h-5" />
             </button>
             <span>{recipe.numberOfLikes}</span>
           </div>
-
           {/* Saves */}
           <div className="flex items-center gap-1 text-body-text">
-            <button
-              className="
-          w-8 h-8 
-          flex items-center justify-center
-          hover:text-primary-text
-          cursor-pointer
-          transition-all duration-200
-          hover:scale-110 active:scale-95
-        "
-            >
+            <button className="w-8 h-8 flex items-center justify-center hover:text-primary-text cursor-pointer transition-all duration-200 hover:scale-110 active:scale-95">
               <BookmarkIcon className="w-5 h-5" />
             </button>
             <span>{recipe.numberOfSaves}</span>
